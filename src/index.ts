8bd1e5ceae0c51d6890169fcbbc8b1759d2f2093
stealthief <josh.siviour@gmail.com>
2024-02-10 14:04:06 +1100

    Setup EJS templates.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.render("test");
 });
 
 app.listen(port, () => {
